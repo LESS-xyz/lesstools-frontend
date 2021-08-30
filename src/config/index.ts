96e41e8a697b1a8cabfeb6d46985847d62939f19
dnotrad <lentsd@bk.ru>
2021-08-30 18:50:51 +0300

    change api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export default {
   APIS: {
-    backend: 'http://144.76.201.50:8018/api/v1/',
-    // backend: 'https://lesstools.rocknblock.io/api/v1/',
+    // backend: 'http://144.76.201.50:8018/api/v1/',
+    backend: 'https://lesstools.rocknblock.io/api/v1/',
   },
 };
