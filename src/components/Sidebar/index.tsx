cdb2835672f1f1cd98314fa7d90f6a2db9fb50f0
Boris Kov <bkovesh@gmail.com>
2021-09-27 19:09:13 +0300

    Sidebar: add accordion

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -6,6 +6,7 @@ import { useMst } from '../../store/store';
 
 import s from './Sidebar.module.scss';
 
+import arrowWhite from '../../assets/img/sections/sidebar/arrow-white.svg';
 import logo from '../../assets/img/sections/sidebar/logo.svg';
 import board from '../../assets/img/sections/sidebar/board.svg';
 // import bot from '../../assets/img/sections/sidebar/bot.svg';
@@ -18,11 +19,36 @@ import pair from '../../assets/img/sections/sidebar/pair.svg';
 import pairWhite from '../../assets/img/sections/sidebar/pair-white.svg';
 import bigSwap from '../../assets/img/sections/sidebar/big-swap.svg';
 import bigSwapWhite from '../../assets/img/sections/sidebar/big-swap-white.svg';
-import uniswap from '../../assets/img/sections/sidebar/uniswap.svg';
-import sushiswap from '../../assets/img/sections/sidebar/suchiswap.svg';
+// import uniswap from '../../assets/img/sections/sidebar/uniswap.svg';
+// import sushiswap from '../../assets/img/sections/sidebar/suchiswap.svg';
+import ethereum from '../../assets/img/sections/sidebar/ethereum.svg';
+import { useState, useCallback } from 'react';
+
+enum Network {
+  Ethereum = 'Ethereum',
+  Binance = 'Binance',
+}
 
 const Sidebar: React.FC = observer(() => {
   const { mobileMenu } = useMst();
+
+  const [activeNetworks, setActiveNetworks] = useState<Network[]>([Network.Ethereum]);
+
+  const handleChangeActiveNetwork = (network: Network) => {
+    let newNetworks: Network[] = [...activeNetworks];
+    if (activeNetworks.includes(network)) {
+      newNetworks = newNetworks.filter((item: Network) => item !== network);
+    } else {
+      newNetworks.push(network);
+    }
+    setActiveNetworks(newNetworks);
+  };
+
+  const isActiveNetwork = useCallback(
+    (item: Network) => activeNetworks.includes(item),
+    [activeNetworks],
+  );
+
   return (
     <aside className={`${s.sidebar} ${mobileMenu.isActive && s.active} grey-scroll`}>
       <NavLink to="/" className={s.logo}>
@@ -36,56 +62,94 @@ const Sidebar: React.FC = observer(() => {
       <div className={s.subtitle}>LESSBOARD</div>
       <LinkSidebar imgDark={board} imgWhite={board} to="/" text="LessBoard" />
       <div className={s.group}>
-        <div className={s.group_title}>
-          <div className={s.group_title__text}>Uniswap</div>
+        <div
+          className={s.group_title}
+          role="button"
+          tabIndex={0}
+          onClick={() => handleChangeActiveNetwork(Network.Ethereum)}
+          onKeyDown={() => {}}
+        >
+          {!mobileMenu.isActive && (
+            <div
+              className={`${s.group_title_arrow} ${
+                isActiveNetwork(Network.Ethereum) && s.group_title_arrow_rotated
+              }`}
+            >
+              <img src={arrowWhite} alt="img" />
+            </div>
+          )}
+          <div className={s.group_title__text}>Ethereum</div>
           <div className={s.group_title__icon}>
-            <img src={uniswap} alt="img" />
+            <img src={ethereum} alt="img" />
           </div>
         </div>
-        <LinkSidebar
-          imgDark={live}
-          imgWhite={liveWhite}
-          to="/uniswap/live-new-pairs"
-          text="Live New Pairs"
-        />
-        <LinkSidebar
-          imgDark={pair}
-          imgWhite={pairWhite}
-          to="/uniswap/pair-explorer/0xa2107fa5b38d9bbd2c461d6edf11b11a50f6b974"
-          text="Pair Explorer"
-        />
-        <LinkSidebar
-          imgDark={bigSwap}
-          imgWhite={bigSwapWhite}
-          to="/uniswap/big-swap-explorer"
-          text="Big Swap Explorer"
-        />
+        {isActiveNetwork(Network.Ethereum) && (
+          <>
+            <LinkSidebar
+              imgDark={live}
+              imgWhite={liveWhite}
+              to="/uniswap/live-new-pairs"
+              text="Live New Pairs"
+            />
+            <LinkSidebar
+              imgDark={pair}
+              imgWhite={pairWhite}
+              to="/uniswap/pair-explorer/0xa2107fa5b38d9bbd2c461d6edf11b11a50f6b974"
+              text="Pair Explorer"
+            />
+            <LinkSidebar
+              imgDark={bigSwap}
+              imgWhite={bigSwapWhite}
+              to="/uniswap/big-swap-explorer"
+              text="Big Swap Explorer"
+            />
+          </>
+        )}
       </div>
       <div className={s.group}>
-        <div className={s.group_title}>
-          <div className={s.group_title__text}>Sushiswap</div>
+        <div
+          className={s.group_title}
+          role="button"
+          tabIndex={0}
+          onClick={() => handleChangeActiveNetwork(Network.Binance)}
+          onKeyDown={() => {}}
+        >
+          {!mobileMenu.isActive && (
+            <div
+              className={`${s.group_title_arrow} ${
+                isActiveNetwork(Network.Binance) && s.group_title_arrow_rotated
+              }`}
+            >
+              <img src={arrowWhite} alt="img" />
+            </div>
+          )}
+          <div className={s.group_title__text}>Binance</div>
           <div className={s.group_title__icon}>
-            <img src={sushiswap} alt="img" />
+            <img src={ethereum} alt="img" />
           </div>
         </div>
-        <LinkSidebar
-          imgDark={live}
-          imgWhite={liveWhite}
-          text="Live New Pairs"
-          to="/sushiswap/live-new-pairs"
-        />
-        <LinkSidebar
-          imgDark={pair}
-          imgWhite={pairWhite}
-          text="Pair Explorer"
-          to="/sushiswap/pair-explorer/0xe06f8d30ac334c857fc8c380c85969c150f38a6a"
-        />
-        <LinkSidebar
-          imgDark={bigSwap}
-          imgWhite={bigSwapWhite}
-          text="Big Swap Explorer"
-          to="/sushiswap/big-swap-explorer"
-        />
+        {isActiveNetwork(Network.Binance) && (
+          <>
+            <LinkSidebar
+              imgDark={live}
+              imgWhite={liveWhite}
+              text="Live New Pairs"
+              to="/sushiswap/live-new-pairs"
+            />
+            <LinkSidebar
+              imgDark={pair}
+              imgWhite={pairWhite}
+              text="Pair Explorer"
+              to="/sushiswap/pair-explorer/0xe06f8d30ac334c857fc8c380c85969c150f38a6a"
+            />
+            <LinkSidebar
+              imgDark={bigSwap}
+              imgWhite={bigSwapWhite}
+              text="Big Swap Explorer"
+              to="/sushiswap/big-swap-explorer"
+            />
+          </>
+        )}
       </div>
       {/* <div className={s.group}>
         <div className={s.subtitle}>common</div>
