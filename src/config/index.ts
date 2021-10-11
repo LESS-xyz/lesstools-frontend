2f5aed74674717e5d5b7cc059197fd72573ffd34
Boris Kov <bkovesh@gmail.com>
2021-10-11 20:13:27 +0300

    Config: add preproduction/production constant

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,14 @@
+const IS_PRODUCTION = false;
+const IS_PREPRODUCTION = true;
+
 export default {
   APIS: {
-    backend: 'https://lesstools.rocknblock.io/api/v1/',
+    // eslint-disable-next-line no-nested-ternary
+    backend: IS_PRODUCTION
+      ? ''
+      : IS_PREPRODUCTION
+      ? 'https://tools.less.xyz/api/v1/'
+      : 'https://lesstools.rocknblock.io/api/v1/',
     COINGECKO: 'https://api.coingecko.com/api/v3',
     // backend: 'https://tools.less.xyz/api/v1/',
     THE_GRAPH: 'https://api.thegraph.com/subgraphs',
